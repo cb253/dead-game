fba25c8ddff5796a95a655e8c71b6d6877320cad
Christian Belarde <cjbelarde253@gmail.com>
2024-01-16 21:14:47 -0800

    FEATURE: Addition of a search bar and basic header

--- deadGame/src/app/app.component.ts
+++ deadGame/src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent,CommonModule, HousingLocationComponent],
+  styleUrl: './app.component.css',
+  template: `
+  <main>
+    <header>
+    <img src="./assets/l"/>
+    <h1>DeadGame</h1>
+    <app-home></app-home>
+    </header>
+    <section>
+      <form>
+        <input type="text" placeholder="Enter game name">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
+  </main>
+  `
 })
 export class AppComponent {
   title = 'deadGame';
+
 }
